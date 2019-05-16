cab90f3c54463f1b4d7564f2a02c939cb06a752c
Deepa <malviya.deepa25@gmail.com>
2019-05-16 17:09:55 +0530

    init

---
+++ app/src/main/java/com/example/dummyapplication/adapter/CategoryAdapter.java
@@ -0,0 +1,76 @@
+package com.example.dummyapplication.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.dummyapplication.R;
+import com.example.dummyapplication.activity.ProductDetailsActivity;
+import com.example.dummyapplication.model.Category;
+
+import java.util.List;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.MyViewHolder> {
+
+    private Context mContext;
+    private List<Category> albumList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title, count;
+        public ImageView thumbnail, overflow;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.title);
+            //  count = (TextView) view.findViewById(R.id.count);
+            thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+            //overflow = (ImageView) view.findViewById(R.id.overflow);
+        }
+    }
+
+
+    public CategoryAdapter(Context mContext, List<Category> albumList) {
+        this.mContext = mContext;
+        this.albumList = albumList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+
+                .inflate(R.layout.row_category, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, int position) {
+        Category album = albumList.get(position);
+        holder.title.setText(album.getName());
+        holder.count.setText(album.getNumOfSongs() + " songs");
+
+        // loading album cover using Glide library
+        Glide.with(mContext).load(album.getThumbnail()).into(holder.thumbnail);
+
+        holder.overflow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, ProductDetailsActivity.class);
+                mContext.startActivity(intent);
+            }
+        });
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return albumList.size();
+    }
+}
+
